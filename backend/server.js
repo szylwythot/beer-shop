1d6e13209eaafd3f67ebd6495ffed39cc215c706
toth_szilvia <szylwy@gmail.com>
2022-04-07 19:10:13 +0200

    bambi

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ function getFunction(request, response){
 app.get("/", getFunction);
 app.use("/frontend/public", express.static(`${__dirname}/../frontend/public`));
 
-const port = 9000;
+const port = 9010;
 const ipAddress = `http://127.0.0.1:${port}`
 app.listen(port, () => {
     console.log(ipAddress)
